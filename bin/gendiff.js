c7dca470f6363da7269654b002193c970c3df501
Kholodetskiy <sergh.yanovskiy@inbox.ru>
2023-08-17 22:13:45 +0500

    update files

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,12 @@
 #!/usr/bin/env node
-
-const { program } = require('commander');
-
+import genDiff from '../src/index.js'
+import { program } from 'commander';
 
 program
-  .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
-  .arguments('<filepath1> <filepath2>')
-  .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format <type>', 'output format')
-  
+.description('Compares two configuration files and shows a difference.')
+.version('0.0.1', '-V, --version', 'output the version number')
+.option('-f, --format <type>', 'output format')
+.arguments('<obj1> <obj2>')
+.action((obj1, obj2) => console.log(genDiff(obj1, obj2)))
+
 program.parse();
