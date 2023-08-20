0f44d5fa0f352532102cb5e5d74f0f2f34957eeb
Kholodetskiy <sergh.yanovskiy@inbox.ru>
2023-08-20 15:08:52 +0500

    update index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,6 +10,6 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixture__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-test('gendiff-JSON', () => {
-  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json')).toEqual(readFile('expected-result.txt'));
+test('genDiff', () => {
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(readFile('expected-result.txt'));
 });
