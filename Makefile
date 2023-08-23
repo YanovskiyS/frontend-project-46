b4de4021af02ff66e34060a5e6352bd0c51cfd99
Kholodetskiy <sergh.yanovskiy@inbox.ru>
2023-08-23 23:16:03 +0500

    update

--- Makefile
+++ Makefile
@@ -2,16 +2,16 @@ install: install-deps
 	npx simple-git-hooks
 
 run:
-	bin/nodejs-package.js 10
+	bin/gendiff.js
 
 install-deps:
 	npm ci
 
 test:
-	NODE_OPTIONS=--experimental-vm-modules npx jest
+	npm test
 
 test-coverage:
-	npx jest --coverage
+	npm test -- --coverage --coverageProvider=v8
 
 lint:
 	npx eslint .
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-import genDiff from '../src/index.js'
 import { program } from 'commander';
+import genDiff from '../src/index.js';
 
 program
-.description('Compares two configuration files and shows a difference.')
-.version('0.0.1', '-V, --version', 'output the version number')
-.option('-f, --format <type>', 'output format')
-.arguments('<obj1> <obj2>')
-.action((obj1, obj2) => console.log(genDiff(obj1, obj2)))
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<obj1> <obj2>')
+  .action((obj1, obj2) => console.log(genDiff(obj1, obj2)));
 
 program.parse();
